6b28c606f36f76dc85f1dd33cf51d3905bfba2f8
RJ_Satyadev <raghavsatyadev@gmail.com>
2020-04-10 17:21:03 +0530

    - added a quick method "showDialogLive" for calling ForceUpdater which is also lifecycle aware

--- moreapps/src/main/java/com/rocky/moreapps/MoreAppsLifecycleListener.java
+++ moreapps/src/main/java/com/rocky/moreapps/MoreAppsLifecycleListener.java
@@ -1,11 +1,26 @@
 package com.rocky.moreapps;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 public interface MoreAppsLifecycleListener {
+    /**
+     * {@link AppCompatActivity} or {@link Fragment} onStart LifeCycle Method
+     */
     void onStart();
 
+    /**
+     * {@link AppCompatActivity} or {@link Fragment} onStop LifeCycle Method
+     */
     void onStop();
 
+    /**
+     * updater dialog is showing, stop other works
+     */
     void showingDialog();
 
+    /**
+     * on completing all processes regarding updater, continue other work
+     */
     void onComplete();
 }
